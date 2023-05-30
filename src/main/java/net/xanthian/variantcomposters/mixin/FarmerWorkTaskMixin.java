0f8008b0af6960f05994be59c4d5c5a99920dc55
xanthian <xanneh@hotmail.com>
2023-05-30 10:37:43 +0100

    added extra mixin for bread making

---
+++ src/main/java/net/xanthian/variantcomposters/mixin/FarmerWorkTaskMixin.java
@@ -0,0 +1,23 @@
+package net.xanthian.variantcomposters.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+
+import net.minecraft.entity.ai.brain.task.FarmerWorkTask;
+
+import net.xanthian.variantcomposters.block.VariantComposterBlock;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(FarmerWorkTask.class)
+public class FarmerWorkTaskMixin {
+
+    @WrapOperation(method = "performAdditionalWork", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"))
+    boolean variantlecterns$customComposter(BlockState state, Block block, Operation<Boolean> original) {
+        return original.call(state, block) || state.getBlock() instanceof VariantComposterBlock;
+    }
+}
